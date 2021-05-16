9ae35e835973c61fd6e3b3a6e562d72bfdb63218
KIMINHO <inho3898@gmail.com>
2021-05-16 22:34:48 +0900

    2021.05.16 KIH First

---
+++ zigzag_task/src/main/java/com/common/util/ZigzagTaskApplication.java
@@ -0,0 +1,13 @@
+package com.common.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZigzagTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZigzagTaskApplication.class, args);
+	}
+
+}
---
+++ zigzag_task/src/test/java/com/common/util/ZigzagTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZigzagTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
